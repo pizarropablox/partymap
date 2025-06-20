2f35cc11565e6553fcb651a04dc3cbac08402d75
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2025-06-20 14:12:58 -0400

    se crea bff para conexion de azure

---
+++ backend/microservicio-bff-main/src/main/java/com/microservicio_bff_main/MicroservicioBffMainApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio_bff_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioBffMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioBffMainApplication.class, args);
+	}
+
+}
---
+++ backend/microservicio-bff-main/src/test/java/com/microservicio_bff_main/MicroservicioBffMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio_bff_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioBffMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
