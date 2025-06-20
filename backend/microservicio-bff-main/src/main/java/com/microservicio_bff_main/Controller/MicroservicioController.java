0822394ea3a86ff39c9fedce1a9c3b13f0242010
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2025-06-20 14:24:35 -0400

    se comienza a configurar bff de conexion con azure

---
+++ backend/microservicio-bff-main/src/main/java/com/microservicio_bff_main/Controller/MicroservicioController.java
@@ -0,0 +1,19 @@
+package com.microservicio_bff_main.controller;    
+
+import java.util.map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/microservicio")
+public class MicroservicioController {
+
+  
+
+}
+
