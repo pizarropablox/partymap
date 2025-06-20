979d903196ed92bb6853e445c5637a2cefc5fe01
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2025-06-20 15:20:31 -0400

    se configura el securityconfig

---
+++ backend/Backend/src/main/java/com/partymap/backend/Config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.partymap.backend.Config;
+
+
+ import org.springframework.context.annotation.Bean;
+ import org.springframework.context.annotation.Configuration;
+ import org.springframework.security.config.Customizer;
+ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+ import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+ import org.springframework.security.web.SecurityFilterChain;
+ /*import org.springframework.security.web.util.matcher.AntPathRequestMatcher;*/
+ import org.springframework.web.client.RestTemplate;
+
+ @Configuration
+ @EnableWebSecurity
+public class SecurityConfig {
+
+     @Bean
+     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+ 
+         http.cors(Customizer.withDefaults()).authorizeHttpRequests(authorize -> authorize
+                        /*.requestMatchers(new AntPathRequestMatcher("/api/xxxxxxx")).permitAll()*/
+                         .anyRequest().authenticated())
+                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+         return http.build();
+     }
+ 
+     @Bean
+     public RestTemplate restTemplate() {
+         return new RestTemplate();
+     }
+
+}
