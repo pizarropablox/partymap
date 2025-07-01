718b49207c1c455b324e6a0409d6e01dd1b8529b
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2025-06-30 22:58:31 -0400

    Delete backend/Backend directory

--- backend/Backend/src/main/java/com/partymap/backend/Repository/ReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.partymap.backend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.partymap.backend.Model.Reserva;
-
-public interface  ReservaRepository  extends JpaRepository<Reserva,Long> {
-
-}
